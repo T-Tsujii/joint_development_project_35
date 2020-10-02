f6dc76a909c2d9997fa482eda9c32d72cd8416d5
keiji <calling.cat.sw@gmail.com>
2020-10-02 23:09:51 +0900

    showを実装

--- app/controllers/messages.controller.rb
+++ app/controllers/messages.controller.rb
@@ -11,6 +11,11 @@ class MessagesController < ApplicationController
     Message.create(message_params)
   end
 
+  def show
+    @message = Message.find(params[:id])
+  end
+
+
   private
 
   def message_params
