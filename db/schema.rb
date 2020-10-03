c220c0f01a116ffb86fa2485fc86957f34e32780
keiji <calling.cat.sw@gmail.com>
2020-10-04 08:03:12 +0900

    メッセージ詳細のh1修正。ファイル名修正、詳細・編集・削除のリンク作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_28_012841) do
+ActiveRecord::Schema.define(version: 2020_10_02_132932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.string "title"
+    t.integer "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "messeages", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
