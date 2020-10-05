56de062b6e33671af071c08f6c510c90efc394ff
keiji <calling.cat.sw@gmail.com>
2020-10-05 22:35:03 +0900

    migrate reset modal migrate　再設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_02_132932) do
+ActiveRecord::Schema.define(version: 2020_10_05_133354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
-    t.integer "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "messeages", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
+    t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
